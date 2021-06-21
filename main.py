b82f314ac3e02301f61a79829210f540fa59fa2d
iskander34 <alex.sosnin34@yandex.ru>
2021-06-21 15:05:47 +0300

    Урок 1. Упражнение 4. в main.py добавлен скрипт GET запроса из задания

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import requests
 
 response = requests.get("https://playground.learnqa.ru/api/hello")
 print(response.text)
+
+response = requests.get("https://playground.learnqa.ru/api/get_text")
+print(response.text)
