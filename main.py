f8b910aea2123d576e4e08770957c2beb361b175
iskander34 <alex.sosnin34@yandex.ru>
2021-06-24 15:37:26 +0300

    Урок 2. Упражнение 10. работа с cookie

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from json.decoder import JSONDecodeError
 import requests
 
+payload = {"login":"secret_login","password":"secret_pass"}
+response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie" , data=payload)
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
-try:
-    parsed_response_text=response.json()
-    print(parsed_response_text)
-except JSONDecodeError:
-    print("Response is not a JSON format")
+cookie_value=response1.cookies.get('auth_cookie')
+
+cookies = {}
+if cookie_value is not None:
+    cookies.update({'auth_cookie': cookie_value})
+
+response2 = requests.post("https://playground.learnqa.ru/api/check_auth_cookie" , cookies=cookies)
+
+print(response2.text)
