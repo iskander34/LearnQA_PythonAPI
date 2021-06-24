84d12d9b0dbc16980e6aa45a52f68f00c7ed4c36
iskander34 <alex.sosnin34@yandex.ru>
2021-06-24 08:05:23 +0300

    Урок 2. Упражнение 4. работа с параметрами

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
-print(response.text)
-
-response = requests.get("https://playground.learnqa.ru/api/get_text")
+payload={"name": "User"}
+response = requests.get("https://playground.learnqa.ru/api/hello",params=payload)
 print(response.text)
