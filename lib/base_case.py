6b516a7bb1204147afb520e1e9a5bab2dabdb6c1
iskander34 <alex.sosnin34@yandex.ru>
2021-09-08 14:05:18 +0300

    Fix grammatical error.

--- lib/base_case.py
+++ lib/base_case.py
@@ -14,7 +14,7 @@ class BaseCase:
         try:
             response_as_dict = response.json()
         except json.decoder.JSONDecodeError:
-            assert False, f"Response is notin JSON format. Response text is '{response.text}"
+            assert False, f"Response is not in JSON format. Response text is '{response.text}"
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
 
         return response_as_dict[name]
