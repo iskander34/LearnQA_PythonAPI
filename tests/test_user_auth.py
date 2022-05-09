a8bc657b74aea6200539b28cd60ce035e900bcf1
iskander34 <alex.sosnin34@yandex.ru>
2022-05-09 20:41:00 +0300

    Ex 19_1

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,9 +1,12 @@
 import pytest
+import allure
+
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
 
+@allure.epic("Authorization cases")
 class TestUserAuth(BaseCase):
     exclude_params = [
         "no_cookie",
@@ -22,6 +25,7 @@ class TestUserAuth(BaseCase):
         self.token = self.get_header(response1, "x-csrf-token")
         self.user_id_from_auth_metod = self.get_json_value(response1, "user_id")
 
+    @allure.description("This tests succesfully authorize user by email and password")
     def test_user_auth(self):
 
         response2 = MyRequests.get(
@@ -37,6 +41,7 @@ class TestUserAuth(BaseCase):
             "User id from auth method is not equal to user id from check method"
         )
 
+    @allure.description("This test checks authorization status w/o sending auth cookie or token")
     @pytest.mark.parametrize('condition', exclude_params)
     def test_negative_auth_check(self, condition):
 
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,6 +1,7 @@
 import pytest
 import string
 import random
+import allure
 
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
@@ -8,6 +9,7 @@ from lib.assertions import Assertions
 from datetime import datetime
 
 
+@allure.epic("User register cases")
 class TestUserRegister(BaseCase):
     exclude_params = [
         "password",
@@ -27,6 +29,7 @@ class TestUserRegister(BaseCase):
         random_part = datetime.now().strftime("%m%d%Y%H%M%S")
         self.email = f"{base_part}{random_part}@{domain}"
 
+    @allure.description("This test succesfully register user after authorize")
     def test_create_user_successfuly(self):
         data = self.prepare_registration_data(None)
 
@@ -35,6 +38,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, "id")
 
+    @allure.description("This test checks register status with sending existing email")
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
         data = self.prepare_registration_data(email)
@@ -45,6 +49,7 @@ class TestUserRegister(BaseCase):
         assert response.content.decode(
             "utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
 
+    @allure.description("This test checks register status w/o sending @ in email")
     def test_create_user_with_out_ad(self):
         base_part = "learnqa"
         domain = "example.com"
@@ -65,6 +70,7 @@ class TestUserRegister(BaseCase):
             "utf-8") == "Invalid email format", f"Unexpected response content {response.content}"
 
     @pytest.mark.parametrize('no_condition', exclude_params)
+    @allure.description("This test checks register status w/o sending one of the necessary parameters")
     def test_create_user_with_out_part(self, no_condition):
         if no_condition == "password":
             data = {
@@ -109,6 +115,7 @@ class TestUserRegister(BaseCase):
             "utf-8") == f"The following required params are missed: {no_condition}", f"Unexpected response content {response.content}"
 
     @pytest.mark.parametrize('name_params', name_params)
+    @allure.description("This test checks register status with sending short/long simbol in name")
     def test_create_user_with_bad_name(self, name_params):
         if name_params == "one_symbol":
             len_name = 1
